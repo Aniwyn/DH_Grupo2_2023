7f199d3e24d0250a5e5c9c9e63d5d9e924f413a4
Aniwyn <kanashiiguree@gmail.com>
2023-07-27 12:18:08 -0300

    cambios en node

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const port = 5000
 
 app.use(express.static("public"))
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 })
 
-app.get("/details", (req, res) =>{
+app.get("/details", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/details.html"))
 })
 
-app.get("/join", (req, res) =>{
+app.get("/join", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/join.html"))
 })
 
-app.get("/login", (req, res) =>{
+app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 })
 
-app.get("/shopping_cart", (req, res) =>{
+app.get("/shopping_cart", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/shopping_cart.html"))
 })
 
-
-const port = 5000
-
 app.listen(port, () => {
-    console.log(`servidor en el puerto ${port}`);
+    console.log(`[server] corriendo en el puerto ${port} (http://localhost:5000/)`);
 })
