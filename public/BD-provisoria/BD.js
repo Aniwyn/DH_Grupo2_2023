03147e0533c29e1017b6054812c4be014e1d119e
Tiano2012 <42954357@fi.unju.edu.ar>
2023-08-18 01:11:23 -0300

    Añadiendo al cart el primer partial, añadiendo ruta de la BD provisoria con un elemento guia, agregando el include dentro del cart

--- public/BD-provisoria/BD.js
+++ public/BD-provisoria/BD.js
@@ -1,3 +1,20 @@
-/* BD */
+const icon_plataform = {
+    PC: '<i class="fa-brands fa-windows"></i>',
+    PS: '<i class="fa-brands fa-playstation"></i>',
+    XBOX: '<i class="fa-brands fa-xbox"></i>',
+    /* AGREGAR MAS ICONOS SEGUN SE NECESITE */
+}
 
+/* BD       voy a crear uno solo para que se hagan de guia*/
+const product = [
+    {
+        name: 'STARFIELD PREMIUM EDITION (Early Access) + PRE-ORDER BONUS PC',
+        description: 'Starfield is the first new universe in over 25 years from Bethesda Game Studios, the award-winning creators of The Elder Scrolls V: Skyrim and Fallout 4. In this next generation role-playing game set amongst the stars, create any character you want and explore with unparalleled freedom as you embark on an epic journey to answer humanity’s greatest mystery.',
+        image: '/image/caratulas/Starfield.jpg',
+        price: '$29,236.59',
+        plataform: [icon_plataform.PC],
+        /* aca agregar si faltan mas */
+    },
+]
 /* EXPORTS */
+module.exports = product
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,12 @@
 /* VARIABLE DECLARATION */
 const path = require("path");
+console.log(__dirname)
+const BD_provisoria = require(path.join(__dirname, "../../public/BD-provisoria/BD"));
 
 /* GETS SET */
 const productController = {
     mycart : (req,res) => {
-        res.render(path.join(__dirname, "../views/products/shopping_cart.ejs"));
+        res.render(path.join(__dirname, "../views/products/shopping_cart.ejs"), {BD: BD_provisoria});
     },
     details : (req,res) => {
         let idPag = req.params.id;
