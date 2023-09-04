694e48c5dbe7201a3f29f1ff8e6f736d4edd0f35
Aniwyn <kanashiiguree@gmail.com>
2023-09-03 21:30:44 -0300

    /products/:id/edit (GET)

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -8,7 +8,8 @@ const productController = require('../controllers/productController');
 /* ROUTE-CONTROLLER CONNECTION */
 router.get('/details/:id', productController.details);
 router.get('/mycart', productController.mycart);
-router.get('/edit_pruduct', productController.editProduct);
+router.get('/create', productController.editProduct);
+router.get('/details/:id/edit', productController.editProduct);
 
 /* EXPORTS */
 module.exports = router;
