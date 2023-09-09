8fc0b905691dab32c9b1a9cb05f5541c53be880a
LeoosMG <leoos.m.e.2019@gmail.com>
2023-09-09 11:26:22 -0300

    agregando Post

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -28,6 +28,10 @@ router.put('/details/:id/edit',upload.fields([
     {name: 'image-cover', maxCount: 1},
     {name: 'image-gameplay', maxCount: 1}
 ]), productController.editProduct_modify);
+router.post('/create',upload.fields([
+    {name: 'image-cover', maxCount: 1},
+    {name: 'image-gameplay', maxCount: 1}
+]), productController.editProduct_post);
 
 router.delete('/details/:id', productController.delete);
 
