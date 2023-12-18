5962c359a996e6f127a5e134b6a800082d7f95b8
Aniwyn <kanashiiguree@gmail.com>
2023-12-18 01:12:44 -0300

    Corregido importaciones

--- src/Data/BD.js
+++ src/Data/BD.js
@@ -1,13 +1,5 @@
-const path = require("path")
-const fs = require("fs")
-const filePathProduct = path.join(__dirname, "/product.json")
-const filePathUsers = path.join(__dirname, "/users.json")
-
-let product = JSON.parse(fs.readFileSync(filePathProduct, "utf-8"))
-let users = JSON.parse(fs.readFileSync(filePathUsers, "utf-8"))
-
 const dato = {
-    id: product[product.length - 1].id + 1,
+    id: 0,
     name: "",
     second_name: "",
     description_1: "",
@@ -36,12 +28,4 @@ const dato = {
 }
 
 
-module.exports = { 
-    product,
-    dato,
-    users 
-    };
-
-
-
-
+module.exports = { dato };
